30ee47eba12d6acd355c41e44462e70dc52fc706
Alex Wang <lifengawang@gmail.com>
2019-12-04 13:51:08 -0500

    agent files for testing

---
+++ code/agent.py
@@ -0,0 +1,12 @@
+import sys
+
+class Agent:
+	def __init__(self, strategies, probabilities):
+		self.strategies = strategies
+		self.probabilities = probabilities
+
+	def play(self):
+		# pick a strategy with its respective probability
+		pass
+
+	
---
+++ code/agent_utils.py
@@ -0,0 +1,4 @@
+from agent import Agent
+
+def play_game(agent_a, agent_b, trials=100):
+	pass
